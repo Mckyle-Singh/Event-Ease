79e373995f5fa40a83337842b8f9543ba5882ece
User Name <mckylesingh71@gmail.com>
2025-03-15 17:10:52 +0200

    feat/users can now add an event

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,58 @@
+﻿using Event_Ease.Data;
+using Event_Ease.Models.Entities;
+using Event_Ease.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Event_Ease.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EventsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var viewModel = new AddEventViewModel
+            {
+                Venues = dbContext.Venues
+            .Where(v => !dbContext.Events.Any(e =>
+                e.VenueID == v.VenueID &&
+                (e.EventStartDate <= DateTime.Today && e.EventEndDate >= DateTime.Today))) // Replace with custom logic if needed
+            .Select(v => new SelectListItem
+            {
+                Value = v.VenueID.ToString(),
+                Text = v.VenueName
+            })
+            .ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEventViewModel viewModel)
+        {
+            var Userevent = new Event
+            {
+                EventName = viewModel.EventName,
+                EventStartDate = viewModel.EventStartDate,
+                EventEndDate = viewModel.EventEndDate,
+                Description = viewModel.Description,
+                VenueID = viewModel.VenueID, 
+
+            };
+            await dbContext.Events.AddAsync(Userevent);
+
+            await dbContext.SaveChangesAsync();
+
+            return RedirectToAction("List", "Venues");
+
+        }
+    }
+}
---
+++ Models/ViewModels/AddEventViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Event_Ease.Models.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Event_Ease.Models.ViewModels
+{
+    public class AddEventViewModel
+    {
+        public string EventName { get; set; } // Event name
+        public DateTime EventStartDate { get; set; } // Event date
+        public DateTime EventEndDate { get; set; } // Event date
+        public string Description { get; set; }//Description of Event
+
+        // Nullable foreign key for Venue
+        // Navigation property (optional)
+        public Guid? VenueID { get; set; }
+        public List<SelectListItem> Venues { get; set; } // List of venues for dropdown options
+
+    }
+}
