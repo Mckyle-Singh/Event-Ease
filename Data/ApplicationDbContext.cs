702a1c1b16956374a1d71efa02ee53afc668ea5e
User Name <mckylesingh71@gmail.com>
2025-03-21 21:26:23 +0200

    Users can delete a venue linked to event

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -14,5 +14,16 @@ namespace Event_Ease.Data
         public DbSet<Event> Events { get; set; }
         public DbSet<Booking> Bookings { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure the Event -> Venue relationship
+            modelBuilder.Entity<Event>()
+                .HasOne(e => e.Venue)
+                .WithMany(v => v.Events)
+                .HasForeignKey(e => e.VenueID)
+                .OnDelete(DeleteBehavior.SetNull); // Set VenueID to null if Venue is deleted
+        }
+
+
     }
 }
---
+++ Migrations/20250321191910_ConfigureDeleteBehavior.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Event_Ease.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigureDeleteBehavior : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Venues_VenueID",
+                table: "Events");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Venues_VenueID",
+                table: "Events",
+                column: "VenueID",
+                principalTable: "Venues",
+                principalColumn: "VenueID",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Venues_VenueID",
+                table: "Events");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Venues_VenueID",
+                table: "Events",
+                column: "VenueID",
+                principalTable: "Venues",
+                principalColumn: "VenueID");
+        }
+    }
+}
