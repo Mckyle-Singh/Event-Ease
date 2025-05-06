08aa4740a87e6255a59f6bacde6a699499f9c915
Mckyle-Singh <80856342+Mckyle-Singh@users.noreply.github.com>
2025-05-06 18:25:53 +0200

    Feat/improve filter (#27)
    * Added filters for venues
    
    * Added clear filter button for venues
    
    * Added filters for events
    
    * Added filters for bookings
    
    * Fixed venue filter for location

--- Controllers/VenuesController.cs
+++ Controllers/VenuesController.cs
@@ -74,6 +74,12 @@ namespace Event_Ease.Controllers
                         break;
                 }
             }
+            // Filter by exact location (from dropdown)
+            if (!string.IsNullOrWhiteSpace(location))
+            {
+                venuesQuery = venuesQuery.Where(v => v.Location == location);
+            }
+            
             var venues = await venuesQuery.ToListAsync();
            
             return View(venues);
