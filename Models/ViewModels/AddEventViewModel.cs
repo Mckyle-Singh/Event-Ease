e546e79f358590f1f79c378f94ed97855a520648
User Name <mckylesingh71@gmail.com>
2025-05-07 18:03:47 +0200

    Fixed event validation messages

--- Models/ViewModels/AddEventViewModel.cs
+++ Models/ViewModels/AddEventViewModel.cs
@@ -1,13 +1,20 @@
 ﻿using Event_Ease.Models.Entities;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace Event_Ease.Models.ViewModels
 {
     public class AddEventViewModel
     {
+        [Display(Name = "Event Name")]
         public string EventName { get; set; } // Event name
+
+        [Display(Name = "Event Start Date")]
         public DateTime EventStartDate { get; set; } // Event date
+
+        [Display(Name = "Event End Date")]
         public DateTime EventEndDate { get; set; } // Event date
+
         public string Description { get; set; }//Description of Event
 
         // Nullable foreign key for Venue
