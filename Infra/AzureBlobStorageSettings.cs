47ba8877bd1c279a20128adab72b9fda5ccbe8a6
User Name <mckylesingh71@gmail.com>
2025-05-13 21:48:21 +0200

    added azure blob settings

---
+++ Infra/AzureBlobStorageSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Event_Ease.Infra
+{
+    public class AzureBlobStorageSettings
+    {
+       
+            public string ConnectionString { get; set; }
+            public string ContainerName { get; set; }
+        
+    }
+}
