708774b33f18adb228b78c5d496b710743892ae4
User Name <mckylesingh71@gmail.com>
2025-05-13 21:54:33 +0200

    Injected Blob storage

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using Event_Ease.Data;
+using Event_Ease.Infra;
+using Event_Ease.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace Event_Ease
@@ -15,6 +17,14 @@ namespace Event_Ease
             //Inject db context
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
+            // Inject AzureBlobStorage settings
+            builder.Services.Configure<AzureBlobStorageSettings>(
+                builder.Configuration.GetSection("AzureBlobStorage")
+            );
+
+            // Inject the blob storage service
+            builder.Services.AddScoped<IBlobStorageService, BlobStorageService>();
 
             var app = builder.Build();
 
